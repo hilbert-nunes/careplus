2a032e2895abe2c310466ed2c834636119117dfd
Hilbert Nunes <hilbert.nunes.dev@gmail.com>
2024-08-05 20:14:22 -0300

    add first part login page

---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+    Form
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/lib/validation"
+
+export enum FormFieldType {
+    INPUT = 'input',
+    EMAIL = 'email',
+    TEXTAREA = 'textarea',
+    PHONE_INPUT = 'phoneInput',
+    CHECKBOX = 'checkbox',
+    DATE_PICKER = 'datePicker',
+    SELECT = 'select',
+    SKELETON = 'skeleton',
+}
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+})
+
+const PatientForm = () => {
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+            name: "",
+            email: "",
+            phone: ""
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof UserFormValidation>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+
+                <section className="mb-12 space-y-4">
+                    <h1 className="header">Hi there👋</h1>
+                    <p className="text-dark-700">Schedule your first appointment</p>
+                </section>
+
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    name="name"
+                    label="Full name"
+                    placeholder="John Doe"
+                    iconSrc="/assets/icons/user.svg"
+                    iconAlt="user"
+                    control={form.control}
+                />
+
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    name="email"
+                    label="Email"
+                    placeholder="johndoe@armazemparaiba.com.br"
+                    iconSrc="/assets/icons/email.svg"
+                    iconAlt="email"
+                    control={form.control}
+                />
+
+                <CustomFormField
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    name="phone"
+                    label="Phone number"
+                    placeholder="(555) 441-3112"
+                    iconSrc="/assets/icons/email.svg"
+                    iconAlt="email"
+                    control={form.control}
+                />
+
+
+                <SubmitButton isLoading={isLoading}>
+                    Get Started
+                </SubmitButton>
+
+            </form>
+        </Form>
+    )
+
+}
+
+export default PatientForm
