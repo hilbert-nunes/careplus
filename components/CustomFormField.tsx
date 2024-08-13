649b1de12c6c203c40bc2e8de04fcd14d9d05294
Hilbert Nunes <hilbert.nunes.dev@gmail.com>
2024-08-12 22:05:46 -0300

    add personal information section in registerForm

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -15,6 +15,9 @@ import { FormFieldType } from './forms/PatientForm'
 import Image from 'next/image'
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Select, SelectContent, SelectTrigger, SelectValue } from './ui/select';
 
 interface CustomProps {
     control: Control<any>;
@@ -33,7 +36,7 @@ interface CustomProps {
 
 const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
 
-    const { fieldType, iconSrc, iconAlt, placeholder } = props;
+    const { fieldType, iconSrc, iconAlt, placeholder, showTimeSelect, dateFormat, renderSkeleton } = props;
 
     switch (fieldType) {
         case FormFieldType.INPUT:
@@ -74,7 +77,45 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
                     />
                 </FormControl>
             );
-
+        case FormFieldType.DATE_PICKER:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    <Image
+                        src="/assets/icons/calendar.svg"
+                        height={24}
+                        width={24}
+                        alt="user"
+                        className="ml-2"
+                    />
+                    <FormControl>
+                        <DatePicker
+                            showTimeSelect={props.showTimeSelect ?? false}
+                            selected={field.value}
+                            onChange={(date) => field.onChange(date)}
+                            timeInputLabel="Time:"
+                            dateFormat={props.dateFormat ?? "MM/dd/yyyy"}
+                            wrapperClassName="date-picker"
+                        />
+                    </FormControl>
+                </div>
+            );
+        case FormFieldType.SKELETON:
+            return props.renderSkeleton ? props.renderSkeleton(field) : null;
+        case FormFieldType.SELECT:
+            return (
+                <FormControl>
+                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                            <SelectTrigger className="shad-select-trigger">
+                                <SelectValue placeholder={props.placeholder} />
+                            </SelectTrigger>
+                        </FormControl>
+                        <SelectContent className="shad-select-content">
+                            {props.children}
+                        </SelectContent>
+                    </Select>
+                </FormControl>
+            );
         default:
             break;
     }
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,13 +1,22 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { users } from "../appwrite.config";
 
+import {
+    BUCKET_ID,
+    DATABASE_ID,
+    ENDPOINT,
+    PATIENT_COLLECTION_ID,
+    PROJECT_ID,
+    databases,
+    storage,
+    users,
+} from "../appwrite.config";
+import { parseStringify } from "@/utils/lib/utils";
+
+// CREATE APPWRITE USER
 export const createUser = async (user: CreateUserParams) => {
     try {
-
-        console.log(user)
-
         // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
         const newuser = await users.create(
             ID.unique(),
@@ -16,14 +25,31 @@ export const createUser = async (user: CreateUserParams) => {
             undefined,
             user.name
         );
-    } catch (error: any) {
 
+        return parseStringify(newuser);
+    } catch (error: any) {
+        // Check existing user
         if (error && error?.code === 409) {
-            const documents = await users.list([
-                Query.equal('email', [user.email]),
+            const existingUser = await users.list([
+                Query.equal("email", [user.email]),
             ]);
 
-            return documents?.users[0]
+            return existingUser.users[0];
         }
+        console.error("An error occurred while creating a new user:", error);
     }
-}
+};
+
+// GET USER
+export const getUser = async (userId: string) => {
+    try {
+        const user = await users.get(userId);
+
+        return parseStringify(user);
+    } catch (error) {
+        console.error(
+            "An error occurred while retrieving the user details:",
+            error
+        );
+    }
+};
